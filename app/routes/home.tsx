2a3c8c6feaa7d27d684cb41e4ee69d529e595acc
rygramer <ryangmercer@gmail.com>
2025-09-25 21:51:04 -0400

    home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,46 +1,28 @@
-import * as React from 'react';
-import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-
-const drawerWidth = 240;
-
-const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen
-  }),
-  overflowX: 'hidden'
-});
-
-const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen
-  }),
-  overflowX: 'hidden',
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(8)} + 1px)`
-  }
-});
+import { styled, useTheme } from '@mui/material/styles';
+import {
+  Box,
+  Drawer,
+  AppBar,
+  Toolbar,
+  List,
+  CssBaseline,
+  Typography,
+  Divider,
+  IconButton,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Paper
+} from '@mui/material';
+import {
+  Menu as MenuIcon,
+  ChevronLeft as ChevronLeftIcon,
+  MoveToInbox as InboxIcon,
+  Mail as MailIcon
+} from '@mui/icons-material';
+import { Fragment, useState } from 'react';
+import { Route } from './+types/home';
 
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -51,61 +33,76 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   ...theme.mixins.toolbar
 }));
 
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
+export function meta({}: Route.MetaArgs) {
+  return [{ title: 'Home' }, { name: 'description', content: 'Welcome home!' }];
 }
 
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open'
-})<AppBarProps>(({ theme }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen
-  }),
-  variants: [
+export default function MiniDrawer() {
+  const menuGroups = [
     {
-      props: ({ open }) => open,
-      style: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.enteringScreen
-        })
-      }
-    }
-  ]
-}));
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== 'open'
-})(({ theme }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: 'nowrap',
-  boxSizing: 'border-box',
-  variants: [
-    {
-      props: ({ open }) => open,
-      style: {
-        ...openedMixin(theme),
-        '& .MuiDrawer-paper': openedMixin(theme)
-      }
+      items: [
+        { text: 'Inbox', icon: <InboxIcon /> },
+        { text: 'Starred', icon: <MailIcon /> },
+        { text: 'Send email', icon: <InboxIcon /> },
+        { text: 'Drafts', icon: <MailIcon /> }
+      ]
     },
     {
-      props: ({ open }) => !open,
-      style: {
-        ...closedMixin(theme),
-        '& .MuiDrawer-paper': closedMixin(theme)
-      }
+      items: [
+        { text: 'All mail', icon: <InboxIcon /> },
+        { text: 'Trash', icon: <MailIcon /> },
+        { text: 'Spam', icon: <InboxIcon /> }
+      ]
     }
-  ]
-}));
+  ];
 
-export default function MiniDrawer() {
+  function Menu({ showText }: { showText: boolean }) {
+    return (
+      <nav>
+        {menuGroups.map((group, groupIdx) => (
+          <Fragment key={groupIdx}>
+            <List>
+              {group.items.map((item, itemIdx) => (
+                <ListItem
+                  key={item.text}
+                  disablePadding
+                  sx={{ display: 'block' }}
+                >
+                  <ListItemButton
+                    sx={{
+                      justifyContent: showText ? 'flex-start' : 'center',
+                      minHeight: 48
+                    }}
+                    aria-labelledby={'menu-item-' + groupIdx + itemIdx}
+                  >
+                    <ListItemIcon
+                      sx={{
+                        minWidth: 0,
+                        justifyContent: 'center',
+                        mr: showText ? 3 : 0
+                      }}
+                      aria-label={item.text}
+                    >
+                      {item.icon}
+                    </ListItemIcon>
+                    {showText && (
+                      <ListItemText
+                        id={'menu-item-' + groupIdx + itemIdx}
+                        primary={item.text}
+                      />
+                    )}
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+            {groupIdx < menuGroups.length - 1 && <Divider />}
+          </Fragment>
+        ))}
+      </nav>
+    );
+  }
   const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -114,146 +111,87 @@ export default function MiniDrawer() {
   const handleDrawerClose = () => {
     setOpen(false);
   };
-
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar position='fixed' open={open}>
+      <AppBar position='fixed'>
         <Toolbar>
           <IconButton
             color='inherit'
             aria-label='open drawer'
             onClick={handleDrawerOpen}
             edge='start'
-            sx={[
-              {
-                marginRight: 5
-              },
-              open && { display: 'none' }
-            ]}
+            sx={{ marginRight: 5 }}
           >
             <MenuIcon />
           </IconButton>
           <Typography variant='h6' noWrap component='div'>
-            Mini variant drawer
+            Hybrid Mini/Temporary Drawer
           </Typography>
         </Toolbar>
       </AppBar>
-      <Drawer variant='permanent' open={open}>
+      {/* Mini sidebar with transition animation, hidden when drawer is open */}
+      <Drawer
+        variant='permanent'
+        open={!open}
+        sx={{
+          width: theme.spacing(7),
+          flexShrink: 0
+        }}
+        slotProps={{
+          paper: {
+            sx: {
+              width: theme.spacing(7),
+              boxSizing: 'border-box',
+              transition: theme.transitions.create(['width', 'margin'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen
+              }),
+              boxShadow: theme.shadows[4],
+              position: 'fixed',
+              top: theme.spacing(8),
+              height: `calc(100vh - ${theme.spacing(8)})`,
+              zIndex: theme.zIndex.drawer,
+              overflowX: 'hidden',
+              ...(open && { width: 0, marginLeft: `-${theme.spacing(7)}` }),
+              [theme.breakpoints.down('sm')]: {
+                top: theme.spacing(7),
+                height: `calc(100vh - ${theme.spacing(7)})`
+              }
+            }
+          }
+        }}
+      >
+        {!open && <Menu showText={false} />}
+      </Drawer>
+      {/* Temporary drawer overlays content with icons and text */}
+      <Drawer
+        anchor='left'
+        open={open}
+        onClose={handleDrawerClose}
+        variant='temporary'
+        ModalProps={{ keepMounted: true }}
+        sx={{ zIndex: theme.zIndex.drawer }}
+        slotProps={{
+          paper: {
+            component: (props) => (
+              <Paper
+                {...props}
+                sx={{ borderRadius: 0 }}
+                aria-label='expanded menu'
+              />
+            )
+          }
+        }}
+      >
         <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? (
-              <ChevronRightIcon />
-            ) : (
-              <ChevronLeftIcon />
-            )}
+          <IconButton onClick={handleDrawerClose} aria-label='close menu'>
+            <ChevronLeftIcon />
           </IconButton>
         </DrawerHeader>
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={[
-                  {
-                    minHeight: 48,
-                    px: 2.5
-                  },
-                  open
-                    ? {
-                        justifyContent: 'initial'
-                      }
-                    : {
-                        justifyContent: 'center'
-                      }
-                ]}
-              >
-                <ListItemIcon
-                  sx={[
-                    {
-                      minWidth: 0,
-                      justifyContent: 'center'
-                    },
-                    open
-                      ? {
-                          mr: 3
-                        }
-                      : {
-                          mr: 'auto'
-                        }
-                  ]}
-                >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText
-                  primary={text}
-                  sx={[
-                    open
-                      ? {
-                          opacity: 1
-                        }
-                      : {
-                          opacity: 0
-                        }
-                  ]}
-                />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={[
-                  {
-                    minHeight: 48,
-                    px: 2.5
-                  },
-                  open
-                    ? {
-                        justifyContent: 'initial'
-                      }
-                    : {
-                        justifyContent: 'center'
-                      }
-                ]}
-              >
-                <ListItemIcon
-                  sx={[
-                    {
-                      minWidth: 0,
-                      justifyContent: 'center'
-                    },
-                    open
-                      ? {
-                          mr: 3
-                        }
-                      : {
-                          mr: 'auto'
-                        }
-                  ]}
-                >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText
-                  primary={text}
-                  sx={[
-                    open
-                      ? {
-                          opacity: 1
-                        }
-                      : {
-                          opacity: 0
-                        }
-                  ]}
-                />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
+        <Menu showText={true} />
       </Drawer>
+      {/* Main content, shifted right to accommodate mini sidebar */}
       <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
         <Typography sx={{ marginBottom: 2 }}>
